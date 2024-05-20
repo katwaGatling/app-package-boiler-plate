eadea38160f01333991e4bd0c61a67e757751a84
katwaGatling <katwa@gatling.io>
2024-05-20 11:26:13 +0200

    update gatling to 3.11.2

--- src/test/java/app/config/TargetEnvResolver.java
+++ src/test/java/app/config/TargetEnvResolver.java
@@ -6,14 +6,8 @@ public class TargetEnvResolver {
 
   public static EnvInfo resolveEnvironmentInfo(String targetEnv) {
     return switch (targetEnv) {
-      case "DEV1" ->
-          new EnvInfo(
-            "INSERT_BASE_URL",
-              "users_dev1.json");
-      default ->
-          new EnvInfo(
-              "INSERT_BASE_URL",
-              "users_dev1.json");
+      case "DEV1" -> new EnvInfo("INSERT_BASE_URL", "users_dev1.json");
+      default -> new EnvInfo("INSERT_BASE_URL", "users_dev1.json");
     };
   }
 }
